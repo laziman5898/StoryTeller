3bb83673a4ff3576b8e5b24aacc8a7fd66af03c3
laziman5898 <78918751+laziman5898@users.noreply.github.com>
2024-10-13 15:11:47 +0100

    git add README.md

---
+++ main.py
@@ -0,0 +1,12 @@
+# Automated Book Generator (Childrens Books, Adult Books etc)
+book_types =  ["Rhyming Book","Informational Books","Poetry Books","Fairytale Books","Folklore Books", "Young Adult Books"]
+genres = ["Fantasy","Horror","Science Fiction","History"]
+
+# Automated Audio Book Generator
+
+#Automated Video Generator
+
+#Automated YouTube Uploader
+
+
+#List the Schedule in a Database
